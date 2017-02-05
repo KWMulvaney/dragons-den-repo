4911dd3da07712aa700bfa5eeb273e4f70632d3a
Emma Lacy <emma@not.public.com>
2017-02-05 11:10:53 +0000

    Added Maven and Travis CI support

--- src/main/java/den/dragon/testClass.java
+++ src/main/java/den/dragon/testClass.java
@@ -1,16 +1,18 @@
-import java.net.*;
-import java.io.*;
+package den.dragon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
 
 public class testClass {
     public static void main(String[] args) throws Exception {
 
         URL oracle = new URL("http://bleb.org/tv/data/listings/-1/4music.xml");
-        BufferedReader in = new BufferedReader(
-        new InputStreamReader(oracle.openStream()));
+        BufferedReader in = new BufferedReader(new InputStreamReader(oracle.openStream()));
 
         String inputLine;
         while ((inputLine = in.readLine()) != null)
             System.out.println(inputLine);
         in.close();
-     }
+    }
 }
